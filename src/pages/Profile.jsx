dac748ada712bf6067e2a5dee0ef1b2fa66bf32a
Rodhis <rodrigonoliva@gmail.com>
2024-07-22 15:00:28 -0300

    FEAT: added profile update form and json-server script

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom"
+import { Button } from "@mui/base"
 
 export default function Profile() {
-
     return (
         <>
-        <nav>
-            <Link to="edit">Edit Profile</Link>
-            <Link to="preferences">Preferences</Link>
-            <Link to="security">Security</Link>
-        </nav>
+            <nav>
+                <Link to="edit">Edit Profile</Link>
+                <Link to="preferences">Preferences</Link>
+                <Link to="security">Security</Link>
+            </nav>
 
-        <form action="submit" method="post">
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" name="last-name" />
-            <label htmlFor="first-name">First Name</label>
-            <input type="text" name="first-name" />
-            <label htmlFor="date-of-birth">Date of Birth</label>
-            <input type="text" name="date " />
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" name="last-name" />
-        </form>
+            <form action="submit" method="post">
+                <label htmlFor="last-name">Last Name</label>
+                <input type="text" name="last-name" />
+                <label htmlFor="first-name">First Name</label>
+                <input type="text" name="first-name" />
+                <label htmlFor="date-of-birth">Date of Birth</label>
+                <input type="date" name="date " />
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" />
+                <label htmlFor="address">Address</label>
+                <input type="text" name="address" />
+                <label htmlFor="country">Country</label>
+                <input type="text" name="country" />
+
+                <Button type="submit">Save</Button>
+            </form>
         </>
     )
 }
