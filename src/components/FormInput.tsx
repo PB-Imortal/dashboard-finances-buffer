02c0e0ac80ea437123e9ae3f029701752fa60251
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-07-22 16:18:30 -0300

    refactor: add properties to FormInput component

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,33 +1,34 @@
-import { Input } from "@mui/base/Input"
+import { Input } from "@mui/base/Input";
+import { ComponentPropsWithRef } from "react";
 
-interface FormInputProps {
-    inputstyles: string
-    labelstyles: string
-    arialabeltext: string
-    placeholdertext: string
-    label: string
-    id: string
+interface FormInputProps extends ComponentPropsWithRef<"input"> {
+  inputstyles?: string;
+  labelstyles?: string;
+  placeholdertext: string;
+  label: string;
+  id: string;
 }
 
 export default function FormInput({
-    inputstyles,
-    labelstyles,
-    arialabeltext,
-    placeholdertext,
-    label,
-    id,
+  inputstyles,
+  labelstyles,
+  placeholdertext,
+  label,
+  id,
+  ...rest
 }: FormInputProps) {
-    return (
-        <div>
-            <label htmlFor={id} className={labelstyles}>
-                {label}
-            </label>
-            <Input
-                id={id}
-                className={inputstyles}
-                placeholder={placeholdertext}
-                aria-label={arialabeltext}
-            ></Input>
-        </div>
-    )
+  return (
+    <div>
+      <label htmlFor={id} className={labelstyles}>
+        {label}
+      </label>
+      <Input
+        id={id}
+        className={inputstyles}
+        placeholder={placeholdertext}
+        aria-label={label}
+        {...rest}
+      ></Input>
+    </div>
+  );
 }
