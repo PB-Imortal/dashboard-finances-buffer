ea007374965d1609b446ff83e3992a6357c99f2b
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-07-23 13:14:16 -0300

    feat: SignIn page

--- src/pages/signinPage.tsx
+++ src/pages/signinPage.tsx
@@ -1,5 +1,23 @@
-import FormCreateAccount from "../components/_molecules/sign-in/FormCreateAccount";
+import SignIn from "../components/_organisms/SignIn";
 
 export default function SignInPage() {
-  return <FormCreateAccount />;
+  return (
+    <section className="h-dvh bg-bgpurple flex flex-col lg:flex-row">
+      <div className="bg-bgpurple flex-1 content-center flex flex-col items-center justify-evenly ">
+        <img
+          className="lg:w-[364px] lg:h-[54px] w-[250px] h-[37px]"
+          src="/src/assets/name-and-logo.svg"
+          alt="Website logo, a black sphere and on the right side the writing My Statement"
+        />
+        <img
+          className="hidden lg:block"
+          src="/src/assets/login-bg.svg"
+          alt=""
+        />
+      </div>
+      <div className="bg-transparent lg:bg-white lg:w-1/2 flex place-content-center">
+        <SignIn />
+      </div>
+    </section>
+  );
 }
