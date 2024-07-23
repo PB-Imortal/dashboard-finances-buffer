c052a190def3cb65cd5838283a7a347e6af00cc2
Rodhis <rodrigonoliva@gmail.com>
2024-07-23 16:22:37 -0300

    STYLE: changed Profile page display to grid

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -21,6 +21,7 @@ export default function Profile() {
     return (
         <>
             <nav className="flex justify-center space-x-4">
+                <Link to="profile">Back</Link>
                 <Link to="edit">Edit Profile</Link>
                 <Link to="preferences">Preferences</Link>
                 <Link to="security">Security</Link>
@@ -29,52 +30,51 @@ export default function Profile() {
             <div className="flex justify-center">
                 <img src={userImage} alt="user avatar" />
             </div>
-            <section className="h-dvh bg-bgwhite flex flex-col items-center lg:flex-row">
-                <form action="submit" method="post" className="flex flex-col space-y-4">
-                    <div className="flex flex-row">
-                        <FormInput
-                            placeholder={"Insert here your last name..."}
-                            label={"Last Name"}
-                            id={"last-name"}
-                        />
-                        <FormInput
-                            placeholder={"Insert here your first name..."}
-                            label={"First Name"}
-                            id={"first-name"}
-                        />
-                    </div>
-                    <FormInput
-                        placeholder={"Insert here your date of birth..."}
-                        label={"Date of Birth"}
-                        id={"date-of-birth"}
-                    />
-                    <FormInput
-                        placeholder={"Insert here your email address..."}
-                        label={"Email"}
-                        id={"email"}
-                    />
-                    <div className="flex flex-row">
-                        <FormInput
-                            placeholder={"Insert here your home address..."}
-                            label={"Address"}
-                            id={"address"}
-                        />
-                        <FormInput
-                            placeholder={"Insert here your residing country..."}
-                            label={"Country"}
-                            id={"country"}
-                        />
-                    </div>
-                    <ButtonComponent
-                        arialabeltext={"send button"}
-                        bgcolor="bgblack"
-                        textColor="txwhite"
-                        styles="h-12 p-4"
-                    >
-                        Save
-                    </ButtonComponent>
-                </form>
-            </section>
+
+            <form action="submit" method="post" className="grid grid-cols-2 gap-x-4 gap-y-5 px-1">
+                <FormInput
+                    placeholder={"Insert here your last name..."}
+                    label={"Last Name"}
+                    id={"last-name"}
+                />
+                <FormInput
+                    placeholder={"Insert here your first name..."}
+                    label={"First Name"}
+                    id={"first-name"}
+                />
+                <FormInput
+                    placeholder={"Insert here your date of birth..."}
+                    label={"Date of Birth"}
+                    id={"date-of-birth"}
+                    styles="col-span-2"
+                />
+                <FormInput
+                    placeholder={"Insert here your email address..."}
+                    label={"Email"}
+                    id={"email"}
+                    styles="col-span-2"
+                />
+
+                <FormInput
+                    placeholder={"Insert here your home address..."}
+                    label={"Address"}
+                    id={"address"}
+                />
+                <FormInput
+                    placeholder={"Insert here your residing country..."}
+                    label={"Country"}
+                    id={"country"}
+                />
+
+                <ButtonComponent
+                    arialabeltext={"send button"}
+                    bgcolor="bgblack"
+                    textColor="txwhite"
+                    styles="h-12 p-4"
+                >
+                    Save
+                </ButtonComponent>
+            </form>
         </>
     )
 }
