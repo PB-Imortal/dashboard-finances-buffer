cc76a2427c8310efcc599f1814c03f3906d28a6b
Andrei Silva <andreiluizsilva07@gmail.com>
2024-07-23 13:54:55 -0300

    feat: Created a separate file as a BurgerMenu component

--- src/router/RootLayout.tsx
+++ src/router/RootLayout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
             {/* <Header /> */}
           
             {/* <SideNav */}
-            
+
             {/* <Main /> */}
             <Outlet />
             {/* <Footer /> */}
