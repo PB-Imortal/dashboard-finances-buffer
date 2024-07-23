cd73dfcb411b2f5f6bbce0e1486f7e69b16669a8
Mateus Maciulevicius <dev.mateusmac@gmail.com>
2024-07-23 17:03:34 -0300

    Feat: login for mobile screen finished

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,25 +6,39 @@ import Title from "../components/_atoms/Title";
 export default function LoginPage() {
   return (
     <main className="font-inter flex flex-col items-center bg-bgpurple">
-      <div className="h-80 w-full flex items-center justify-center" >
-        <img src={statementImg} alt="statement image" className="w-64 h-9"/>
+      <div className="h-72 w-full flex items-center justify-center">
+        <img src={statementImg} alt="statement image" className="w-64 h-9" />
       </div>
-      <section className="flex flex-col p-3 justify-items-center border-solid rounded-t-2xl bg-bgwhite">
-        <div className="flex flex-col items-center">
+      <section className="h-3/4 w-full flex flex-col justify-between p-3  border-solid rounded-t-2xl bg-bgwhite">
+        <div className="flex flex-col gap-y-4 items-center">
           <Title>Log in</Title>
-          <p>
+          <p className="w-72 text-center">
             Welcome to My Statement, please fill in the fields below to log into
             your account.
           </p>
         </div>
-        <div>
-          <FormInput id="email" label="E-mail" placeholder="E-mail"/>
-          <FormInput id="password" label="Password" placeholder="Password"/>
+        <div className="flex flex-col mt-2 gap-y-2">
+          <FormInput id="email" label="E-mail" placeholder="E-mail" />
+          <FormInput id="password" label="Password" placeholder="Password" />
         </div>
-        <div className="flex flex-col justify-items-center justify-between mt-3">
-          <ButtonComponent arialabeltext="Login button" bgcolor="bg-bgblack" textColor="text-txwhite" styles="h-12">Log in</ButtonComponent>
+        <div className="flex flex-col gap-y-2.5 justify-items-center justify-between mt-3">
+          <ButtonComponent
+            arialabeltext="Login button"
+            bgcolor="bg-bgblack"
+            textColor="text-txwhite"
+            styles="h-12"
+          >
+            Log in
+          </ButtonComponent>
           <p className="self-center">Or</p>
-          <ButtonComponent arialabeltext="create account button" bgcolor="bg-bgwhite" textColor="text-txblack" styles="border border-zinc-600/[.35] h-12">Create account</ButtonComponent>
+          <ButtonComponent
+            arialabeltext="create account button"
+            bgcolor="bg-bgwhite"
+            textColor="text-txblack"
+            styles="border border-zinc-600/[.35] h-12"
+          >
+            Create account
+          </ButtonComponent>
         </div>
       </section>
     </main>
