08ceda71e170ac7b6428b5d87952d00d72f165a6
Mateus Maciulevicius <dev.mateusmac@gmail.com>
2024-07-23 11:27:43 -0300

    refactor: editing styles props

--- src/components/_atoms/Button.tsx
+++ src/components/_atoms/Button.tsx
@@ -5,17 +5,21 @@ interface ButtonProps extends ComponentPropsWithRef<"button"> {
   styles?: string;
   arialabeltext: string;
   children?: ReactNode;
+  bgcolor: "bgblack" | "bgwhite"
+  textColor: "txwhite" | "txblack"
 }
 
 export default function ButtonComponent({
   children,
   styles,
   arialabeltext,
+  bgcolor,
+  textColor,
   ...rest
 }: ButtonProps) {
   return (
     <Button
-      className={`p-3 bg-bgblack text-txwhite rounded-md font-semibold ${styles}`}
+      className={`p-3 bg-${bgcolor} text-${textColor} rounded-md font-semibold ${styles}`}
       aria-label={arialabeltext}
       {...rest}
     >
