34078f5a7f63389618ee825e29872d3c60fcb699
Marco Rezon <marcorezonmoura1@gmail.com>
2024-07-24 17:16:27 -0400

    Config: reset Statement redering page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,9 @@
 import NavBar from "../components/_molecules/NavBar";
-import Statement from "../components/_organisms/Statement";
 
 export default function HomePage() {
     return <>
     
-        <div className="bg-bggrey min-h-screen"> <NavBar/> <Statement /></div>
+        <div className="bg-bggrey min-h-screen"> <NavBar/></div>
 
     </>
 }
