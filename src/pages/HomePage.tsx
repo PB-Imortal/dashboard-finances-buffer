8f0f08d265de3203ef686c099d34f79b6ac8a04e
Marco Rezon <marcorezonmoura1@gmail.com>
2024-07-24 17:21:56 -0400

    Style: readded txtpurple, red and green in twind configs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import NavBar from "../components/_molecules/NavBar";
+import Statement from "../components/_organisms/Statement";
 
 export default function HomePage() {
   return (
     <div className="bg-bggrey min-h-screen">
       <NavBar />
+      <Statement />
     </div>
   );
 }
