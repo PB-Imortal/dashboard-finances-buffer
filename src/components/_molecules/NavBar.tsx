c787b3b0ff479830e1194cc2204f1fd87159c970
will.terra <wpterra@outlook.com>
2024-07-24 14:30:49 -0300

    feat: sidebar state

--- src/components/_molecules/NavBar.tsx
+++ src/components/_molecules/NavBar.tsx
@@ -8,13 +8,43 @@ import FormInput from "../_atoms/FormInput";
 import DeskTopSideBar from "../SideBar/DeskTopSideBar";
 import UserProfileIcon from "../common/svg/UserProfileIcon";
 
+import { useState, useEffect } from "react";
+
+const useScreenSize = () => {
+  const [screenSize, setScreenSize] = useState<{width:number}>({
+    width: window.innerWidth,
+    
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+      
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return screenSize;
+};
+
 export default function NavBar() {
+  const screenSize = useScreenSize();
   return (
     <div className="flex flex-row grow sm:bg-white md:bg-inherit">
-      <DeskTopSideBar styles="sm:hidden md:hidden lg:flex xl:flex"/>
-      <SideBar styles="sm:flex md:hidden " />
+      {screenSize.width < 1023 ? (
+        <SideBar styles="sm:flex md:flex lg:hidden xl:hidden " />
+      ) : (
+        <DeskTopSideBar styles="sm:hidden md:hidden lg:flex" />
+      )}
 
-      <div className=" flex mr-4 grow bg-white rounded md:rounded-lg  sm:justify-start justify-around  md:ml-4 sm:px-2 sm:h-[88px] h-[120px]  md:mt-4">
+      <div className=" flex lg:mr-4 grow bg-white rounded md:rounded-lg  sm:justify-start justify-around  lg:ml-4 sm:px-2 sm:h-[88px] h-[120px]  lg:mt-4">
         <FormInput
           id="search"
           aria-label="Search box"
@@ -22,8 +52,6 @@ export default function NavBar() {
           type="text"
           endSvg={<SearchIcon />}
           styles="space-y-0 justify-start  grow lg:ml-12 mt-2"
-     
-      
         >
           {" "}
         </FormInput>
@@ -52,7 +80,7 @@ export default function NavBar() {
               arialabeltext="Profile button"
               bgcolor="bg-bgwhite"
             >
-             <UserProfileIcon/>
+              <UserProfileIcon />
               {/*   será necessário pegar a imagem de usuario do banco de dados */}
             </ButtonComponent>
           </Link>
