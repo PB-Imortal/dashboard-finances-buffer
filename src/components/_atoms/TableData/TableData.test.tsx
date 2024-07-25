f058161c0a5d823d2646c9f4e030f2b874f42de4
Marco Rezon <marcorezonmoura1@gmail.com>
2024-07-25 16:05:24 -0400

    Test: added test to TableHeader atom

--- src/components/_atoms/TableData/TableData.test.tsx
+++ src/components/_atoms/TableData/TableData.test.tsx
@@ -4,19 +4,25 @@ import { TableData } from './TableData'
 
 describe('TableData molecule', () => {
 
-    it('should render children text argument', () => {
+    it('should render children as text', () => {
         render(<TableData>Data</TableData>)
         const tableData = screen.getByText('Data')
         expect(tableData).toBeInTheDocument()
     })
 
-    it('should render children tsx argument', () => {
+    it('should render children as jsx', () => {
         render(<TableData><span data-testid='1'></span></TableData>)
-        const span = screen.getByTestId('1')
-        expect(span).toBeInTheDocument()
+        const child = screen.getByTestId('1')
+        expect(child).toBeInTheDocument()
     })
 
-    it('should include tailwind style to className property', () => {
+    it('should have a classes preset', () => {
+        render(<TableData>Data</TableData>)
+        const tableData = screen.getByText('Data')
+        expect(tableData).toHaveClass('text-left border-t py-3 md:p-2') 
+    })
+
+    it('should include tailwind style to the class attribute', () => {
         render(<TableData variantStyle="class">Data</TableData>)
         const tableData = screen.getByText('Data')
         expect(tableData).toHaveClass('class')
