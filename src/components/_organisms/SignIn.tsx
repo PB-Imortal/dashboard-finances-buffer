82c11379db02119bb50be96e5a9cb9153200cf9f
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-07-25 13:46:37 -0300

    refactor: SignIn page structure

--- src/components/_organisms/SignIn.tsx
+++ src/components/_organisms/SignIn.tsx
@@ -3,7 +3,7 @@ import FormCreateAccount from "../_molecules/sign-in/FormCreateAccount";
 
 export default function SignIn() {
   return (
-    <section className="bg-white space-y-8 max-w-[492px] min-w-[320px] m-auto py-6 px-4 lg:rounded-none rounded-2xl">
+    <section className="bg-white space-y-8 lg:h-full flex-col place-content-center lg:w-[50%] m-auto py-6 px-4 lg:rounded-none rounded-2xl">
       <div className="text-center space-y-8">
         <Title>Create account</Title>
         <p>Please fill in the fields below to create an account.</p>
