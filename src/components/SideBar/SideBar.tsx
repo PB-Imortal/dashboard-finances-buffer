6a5cf65b7a788f1b79d8454943ba2616eba450cd
Andrei Silva <andreiluizsilva07@gmail.com>
2024-07-26 11:52:37 -0300

    WIP:Adjusting the menu spacing to fit the statement

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -13,7 +13,24 @@ import LogoutSideBarIcon from "../../assets/logout-sidebar-icon.svg";
 import LogoIcon from "../../assets/BlackLogo.svg";
 import HomeIconActive from "../../assets/home-active-icon.svg";
 
-import BurgerMenu from "./BurgerMenu";
+interface BurgerMenuProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const BurgerMenu: React.FC<BurgerMenuProps> = ({ isOpen, setIsOpen }) => {
+  return (
+    <button
+      data-testid="burger-menu"
+      className={`my-auto bg-bgwhite px-4 py-9 rounded flex flex-col justify-center items-center gap-1 ${isOpen ? "z-20" : "z-60"}`}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <span className="block w-5 h-0.5 bg-black"></span>
+      <span className="block w-5 h-0.5 bg-black"></span>
+      <span className="block w-5 h-0.5 bg-black"></span>
+    </button>
+  );
+};
 
 interface NavLinkProps {
   to: string;
@@ -43,74 +60,35 @@ const SideBar: React.FC<SideBarProps> = ({ styles }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <div className={`${styles}`}>
-        <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
-        {isOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 z-30" onClick={() => setIsOpen(false)}></div>
-        )}
-        <div className={`fixed top-0 left-0 z-40 w-64 h-full bg-white shadow-md transform ease-in-out duration-300 rounded-tr-[18px] rounded-br-[18px] p-5 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-          <ul>
-            <li className="flex justify-center p-2">
-              <img src={LogoIcon} alt="logo" className="h-auto" />
-            </li>
-            <li className="p-4">
-              <div className="border-t border-#DFDFE0"></div>
-            </li>
-            <li className="p-4">
-              <button className="absolute top-0 right-0 mt-1 mr-3 text-black text-[26px]" onClick={() => setIsOpen(false)}>&#120;</button>
-            </li>
-          </ul>
-          <div className="p-4">
-            <nav className="flex flex-col gap-12 font-[600]">
-
-              <NavLink 
-              to="/" 
-              activeImgSrc={HomeIconActive} 
-              inactiveImgSrc={HomeIconInactive}>
-                Home
-              </NavLink>
-
-              <NavLink 
-              to="/statement" 
-              activeImgSrc={StatementIconActive} 
-              inactiveImgSrc={StatementIconInactive}>
-                Statement
-              </NavLink>
-
-              <NavLink 
-              to="/profile" 
-              activeImgSrc={ProfileIconActive} 
-              inactiveImgSrc={ProfileIconInactive}>
-                Profile
-              </NavLink>
-              <NavLink 
-              
-              to="#"
-              activeImgSrc={NotificationSideBarActive} 
-              inactiveImgSrc={NotificationSideBar}>
-                Notification
-              </NavLink>
-
-              <NavLink 
-              to="#" 
-              activeImgSrc={SettingSideBarActive} 
-              inactiveImgSrc={SettingSideBar}>
-                Setting
-              </NavLink>
-
-              <NavLink 
-              to="/login" 
-              activeImgSrc={LogoutSideBarIcon} 
-              inactiveImgSrc={LogoutSideBarIcon}>
-                Logout
-              </NavLink>
-              
-            </nav>
-          </div>
+    <div className={`${styles}`}>
+      <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-30" onClick={() => setIsOpen(false)}></div>
+      )}
+      <div className={`fixed top-0 left-0 z-40 w-64 h-full bg-white shadow-md transform ease-in-out duration-300 rounded-tr-[18px] rounded-br-[18px] p-5 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
+        <ul>
+          <li className="flex justify-center p-2">
+            <img src={LogoIcon} alt="logo" className="h-auto" />
+          </li>
+          <li className="p-4">
+            <div className="border-t border-#DFDFE0"></div>
+          </li>
+          <li className="p-4">
+            <button className="absolute top-0 right-0 mt-1 mr-3 text-black text-[26px]" onClick={() => setIsOpen(false)}>&#120;</button>
+          </li>
+        </ul>
+        <div className="p-4">
+          <nav className="flex flex-col gap-12 font-[600]">
+            <NavLink to="/" activeImgSrc={HomeIconActive} inactiveImgSrc={HomeIconInactive}>Home</NavLink>
+            <NavLink to="/statement" activeImgSrc={StatementIconActive} inactiveImgSrc={StatementIconInactive}>Statement</NavLink>
+            <NavLink to="/profile" activeImgSrc={ProfileIconActive} inactiveImgSrc={ProfileIconInactive}>Profile</NavLink>
+            <NavLink to="#" activeImgSrc={NotificationSideBarActive} inactiveImgSrc={NotificationSideBar}>Notification</NavLink>
+            <NavLink to="#" activeImgSrc={SettingSideBarActive} inactiveImgSrc={SettingSideBar}>Setting</NavLink>
+            <NavLink to="/login" activeImgSrc={LogoutSideBarIcon} inactiveImgSrc={LogoutSideBarIcon}>Logout</NavLink>
+          </nav>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
