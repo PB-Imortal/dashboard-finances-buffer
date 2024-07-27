091c15ce07ddd62f5cc9848b1995a5233fff93e3
Marco Rezon <marcorezonmoura1@gmail.com>
2024-07-27 00:09:32 -0400

    Test: added tests to HeadBlock atom

---
+++ src/components/_atoms/HeadBlock/HeadBlock.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import { HeadBlock } from './HeadBlock';
+
+const testData = {label: 'Value', icon: 'invalid_url', amount: 313}
+
+describe("HeadBlock atom", () => {
+
+    it('should render', () => {
+        render(<HeadBlock data={testData} />)
+
+        const label = screen.getByText(testData.label)
+        const amount = screen.getByText(`$${testData.amount}`)
+
+        expect(label).toBeInTheDocument()
+        expect(amount).toBeInTheDocument()
+    })
+
+    it('should apply value to img attibutes', () => {
+        render(<HeadBlock data={testData} />)
+
+        const img = screen.getByRole('img')
+
+        expect(img).toHaveAttribute('src', testData.icon)
+        expect(img).toHaveAttribute('alt', `${testData.label} icon`)
+    })
+
+    it('should display the img alt text if the url is not valid', () => {
+        render(<HeadBlock data={testData} />)
+        expect(screen.getByAltText(`${testData.label} icon`)).toBeVisible()
+    })
+})
