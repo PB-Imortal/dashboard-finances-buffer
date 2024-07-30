018e95de752c7c3a2636540dfa440bbd1385323c
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-07-30 14:11:21 -0300

    fix: merge conflicts

--- src/common/functions/validations.ts
+++ src/common/functions/validations.ts
@@ -23,32 +23,12 @@ export const formCreateAccount = z
 export type CreateAccountFields = z.infer<typeof formCreateAccount>;
 
 export const formLogin = z.object({
-    email: z.string().email("Please use a valid e-mail address").min(1, "Required").trim(),
-    password: z.string().min(1, "Required").trim(),
-})
-
-export type FormLoginField = z.infer<typeof formLogin>
-
-export const profileEditForm = z.object({
-  lastName: z
-    .string()
-    .min(2, "Required*")
-    .transform((val) => val.trim()),
-  firstName: z
-    .string()
-    .min(2, "Required*")
-    .transform((val) => val.trim()),
-  dateOfBirth: z
-    .string()
-    .refine((val) => /^\d{4}-\d{2}-\d{2}$/.test(val), "Invalid date format. Expected YYYY-MM-DD")
-    .transform((val) => val.trim()),
-
   email: z
     .string()
     .email("Please use a valid e-mail address")
     .min(1, "Required")
     .trim(),
-  password: z.string().min(1, "Required").trim(), // use refine method later to validate if user really exists based on dummy backend return data
+  password: z.string().min(1, "Required").trim(),
 });
 
 export type FormLoginField = z.infer<typeof formLogin>;
