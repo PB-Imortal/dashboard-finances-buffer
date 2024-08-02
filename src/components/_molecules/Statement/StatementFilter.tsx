5d698c4295bc6eafb3a1ab6fb955f753e6d607c4
Marco Rezon <marcorezonmoura1@gmail.com>
2024-08-01 23:51:56 -0400

    Refactor: reset input of type month to a form

--- src/components/_molecules/Statement/StatementFilter.tsx
+++ src/components/_molecules/Statement/StatementFilter.tsx
@@ -6,16 +6,46 @@ export function StatementFilter() {
   return (
     <div className="flex gap-3 justify-end flex-row">
       <input
-        type="month"
-        className="border-separate p-2 rounded-lg max-w-fit	"
-        onChange={(e) => statementContext.setFilter}
-      />
+        type="text"
+        className="border-separate px-2 rounded-lg max-w-fit"
+        placeholder="Search"
+        onChange={(e) => statementContext.setFilter(e.target.value.toLowerCase())}
+      />     
+      <form>
+        <div className="bg-bgwhite border-separate px-2 rounded-lg max-w-fit">
+          <div className="flex gap-2">
+            <span>
+              <select id="month" name="month">
+                <option selected>January</option>
+                <option>February</option>
+                <option>March</option>
+                <option>April</option>
+                <option>May</option>
+                <option>June</option>
+                <option>July</option>
+                <option>August</option>
+                <option>September</option>
+                <option>October</option>
+                <option>November</option>
+                <option>December</option>
+              </select>
+            </span>
+            <span>
+              <label>Year:</label>
+              <select id="year" name="year"></select>
+            </span>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
-/*      <input 
-        type="text"
-        className="border-separate p-2 rounded-lg max-w-fit" 
-        placeholder="Search"
-        onChange={(e) => statementContext.setFilter(e.target.value.toLowerCase())}  
-      />*/
+/*
+      <input
+        type="month"
+        min="2008-01"
+        max=""
+        className="border-separate px-2 rounded-lg max-w-fit	"
+        onChange={(e) => statementContext.setFilter}
+      />
+*/ 
