bbda66486dd7ae951bf2afdc0b4e3aab199b6519
Marco Rezon <marcorezonmoura1@gmail.com>
2024-08-02 14:35:43 -0400

    Refactor: reset StatementFilter structure

--- src/components/_molecules/Statement/StatementFilter.tsx
+++ src/components/_molecules/Statement/StatementFilter.tsx
@@ -2,50 +2,42 @@ import { useContext } from "react"
 import { StatementContext } from "./apiEntities"
 
 export function StatementFilter() {
+
   const statementContext = useContext(StatementContext)
+
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+
+  function getYearsGap() {
+    let date = new Date
+    const gap = []
+    for(let i = date.getFullYear(); i >= 2008; i--) {gap.push(i)}
+    return gap
+  }
+
+
   return (
-    <div className="flex gap-3 justify-end flex-row">
+    <form className="flex justify-end gap-3 items-center">
+      <button>Filter</button>
+      
       <input
         type="text"
-        className="border-separate px-2 rounded-lg max-w-fit"
+        className="bg-bgwhite border-separate rounded-md px-2"
         placeholder="Search"
-        onChange={(e) => statementContext.setFilter(e.target.value.toLowerCase())}
-      />     
-      <form>
-        <div className="bg-bgwhite border-separate px-2 rounded-lg max-w-fit">
-          <div className="flex gap-2">
-            <span>
-              <select id="month" name="month">
-                <option selected>January</option>
-                <option>February</option>
-                <option>March</option>
-                <option>April</option>
-                <option>May</option>
-                <option>June</option>
-                <option>July</option>
-                <option>August</option>
-                <option>September</option>
-                <option>October</option>
-                <option>November</option>
-                <option>December</option>
-              </select>
-            </span>
-            <span>
-              <label>Year:</label>
-              <select id="year" name="year"></select>
-            </span>
-          </div>
-        </div>
-      </form>
-    </div>
+      />
+
+      <select className="border-separate rounded-md px-2">
+        {months.map(month =>
+            <option key={month} value={month}>{month}</option>)
+        }
+      </select>
+      
+      <select className="border-separate rounded-md px-2">
+        {getYearsGap().map(year => 
+          <option key={year} value={year}>{year}</option>)
+        }
+      </select>
+
+    </form>
   )
 }
-/*
-      <input
-        type="month"
-        min="2008-01"
-        max=""
-        className="border-separate px-2 rounded-lg max-w-fit	"
-        onChange={(e) => statementContext.setFilter}
-      />
-*/ 
