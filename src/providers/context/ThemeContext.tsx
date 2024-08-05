d4d5c74cffa84a233edc8dbf00ea8d9bf27d41eb
Rodhis <rodrigonoliva@gmail.com>
2024-08-05 16:36:25 -0300

    BUG: added comments to ignore a non-necessary eslint warning

--- src/providers/context/ThemeContext.tsx
+++ src/providers/context/ThemeContext.tsx
@@ -36,4 +36,5 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => useContext(ThemeContext);
