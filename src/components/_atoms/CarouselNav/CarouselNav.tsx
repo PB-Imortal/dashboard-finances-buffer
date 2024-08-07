8e69e4a962d22cb7c27eedea10c90b0b985488fd
Rodhis <rodrigonoliva@gmail.com>
2024-08-07 15:56:03 -0300

    REFACTOR: test coverage for carouselnav 100%

--- src/components/_atoms/CarouselNav/CarouselNav.tsx
+++ src/components/_atoms/CarouselNav/CarouselNav.tsx
@@ -49,7 +49,7 @@ const CarouselNav: React.FC<CarouselNavProps> = ({ items }) => {
     }
 
     return (
-        <nav className="relative bg-bgwhite py-[0.95rem] md:-ml-14 lg:-ml-3 dark:bg-dkrbgitenseblue rounded-lg lg:ml-[0.5rem] lg:mr-4">
+        <nav className="relative bg-bgwhite py-[0.95rem] md:-ml-14 dark:bg-dkrbgitenseblue rounded-lg lg:ml-[0.5rem] lg:mr-4">
             <div className="overflow-x-auto whitespace-nowrap flex items-center space-x-4 p-4 scrollbar-hide">
                 {actualItems.map((item, index) => (
                     <div
