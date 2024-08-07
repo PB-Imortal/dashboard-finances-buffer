36999103e8804e205e23c76cb98ae9e927bb7b01
Mateus Maciulevicius <dev.mateusmac@gmail.com>
2024-08-07 15:51:44 -0300

    feat: adding Rodrigo's api test file

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/setupTests.ts"],
-    include: ["src/**/*.test.tsx"],
+    include: ["src/**/*.test.tsx", "src/**/*.test.ts"],
     coverage: {
       exclude: [
         ...configDefaults.coverage.exclude,
