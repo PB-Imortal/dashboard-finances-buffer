7808b50c69f10c249cfcc8209410ece6fec7a240
Marco Rezon <marcorezonmoura1@gmail.com>
2024-08-12 09:13:07 -0400

    Test: added test to FormSelect atom

---
+++ src/components/_atoms/Select/FormSelect.test.tsx
@@ -0,0 +1,37 @@
+import {render, screen} from "@testing-library/react";
+import { FormSelect } from "./FormSelect";
+
+describe("FormStatement", () => {
+    const mockData = {
+        label: "Test",
+        options: ["1", "2"],
+        testId: "01"
+    };
+
+    it("should render props", () => {
+        render(
+            <FormSelect 
+                label={mockData.label} 
+                options={mockData.options} 
+                data-testid={mockData.testId}
+            />);
+        
+        const label = screen.getByText(mockData.label);
+
+        expect(label).toBeInTheDocument();
+        mockData.options.map(option => 
+            expect(screen.getByText(option)).toBeInTheDocument())
+    });
+
+    it("should render options as select children", () => {
+        render(
+            <FormSelect 
+                label={mockData.label} 
+                options={mockData.options} 
+                data-testid={mockData.testId}
+            />
+        );
+        const select = screen.getByTestId(mockData.testId);
+        expect(select.childElementCount).toBe(mockData.options.length)
+    })
+})
