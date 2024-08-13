ce5bd2ceb043d33c81fd53dfadf5b99ab4015958
Marco Rezon <marcorezonmoura1@gmail.com>
2024-08-12 23:17:39 -0400

    Docs: set StatmentContext in StatementTale stories

--- src/components/_molecules/Statement/StatementTable.stories.tsx
+++ src/components/_molecules/Statement/StatementTable.stories.tsx
@@ -1,12 +1,46 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
+import { StatementContext } from './apiEntities';
 import { StatementTable } from './StatementTable';
 
 const meta = {
   title: 'molecules/StatementTable',
   component: StatementTable,
-  tags: ['autodocs']
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <StatementContext.Provider value={{ filteredData  }}>
+          <Story />
+      </StatementContext.Provider>
+    ),
+  ],
 } satisfies Meta<typeof StatementTable>;
+
+const filteredData = [
+    {
+      description: "Spotify",
+      id: "#31426590",
+      type: "Shopping",
+      card: "1241432",
+      date: "28 Jan, 12.30 AM",
+      amount: -2500,
+    },
+    {
+      description: "Freepik Sales",
+      id: "#31426589",
+      type: "Transfer",
+      card: "1241432",
+      date: "28 Jan, 12.30 AM",
+      amount: 750,
+    },
+    {
+      description: "Mobile Service",
+      id: "#31426588",
+      type: "Transfer",
+      card: "1241432",
+      date: "28 Jan, 12.30 AM",
+      amount: -150,
+    },
+  ];
 
 export default meta;
 
